3a4aa2fc7e717748b3dc6252ddaf012ada8b1e1f
Sparsh <sparshmanhas111@gamil.com>
2025-11-18 01:19:54 +0530

    deployement changes

--- plenotube/src/Main.tsx
+++ plenotube/src/Main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.js";
-import { Provider } from "./provider.tsx";
+import { Provider } from "./provider";
 import { AuthProvider } from "./context/AuthContext"
 import { UserProvider } from "./context/UserContext"
 import {CampaignProvider} from "./context/CampaignContext"
